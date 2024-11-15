784a5860cdf01d1e51432a6e830910f0f1165ed3
J180481 <J180481@tafe.wa.edu.au>
2024-11-16 01:03:49 +0800

    feat: added constructors and attributes to the display and sensor classes

--- src/display.py
+++ src/display.py
@@ -2,4 +2,15 @@
 
 
 class Display:
-    pass
+    def __init__(self, id, car_park, message="", is_on=False):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        return f'{self.id} {self.message}'
+
+
+#display1 = Display(id="Display 1:", car_park=2, message="Welcome to the car park", is_on=True)
+#print(display1)
--- src/sensor.py
+++ src/sensor.py
@@ -2,4 +2,17 @@
 
 
 class Sensor:
-    pass
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f'Sensor {self.id} is active: {self.is_active}'
+
+
+class EntrySensor(Sensor):
+    ...
+
+class ExitSensor(Sensor):
+    ...
