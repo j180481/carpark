c320d2dc8e1f71d0eaf33320c3ee9b128fe9384c
J180481 <J180481@tafe.wa.edu.au>
2024-11-19 00:09:18 +0800

    feat: saved json config and altered main file to load from the saved config file

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from car_park import CarPark
+from sensor import EntrySensor, ExitSensor
+from display import Display
+
+#car_park = CarPark("Moondalup", 100, log_file="moondalup.txt")
+
+#car_park.write_config()
+
+car_park = CarPark.load_config()
+
+entry_sensor = EntrySensor(identity=1, is_active=True, car_park=car_park)
+
+exit_sensor = ExitSensor(identity=2, is_active=True, car_park=car_park)
+
+display = Display(identity=1, message="Welcome to Moondalup", is_on=True, car_park=car_park)
+print(display)
+
+car_park.register(entry_sensor)
+car_park.register(exit_sensor)
+car_park.register(display)
+
+for i in range(10):
+    entry_sensor.detect_vehicle()
+
+for i in range(2):
+    exit_sensor.detect_vehicle()
