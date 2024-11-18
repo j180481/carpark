c2d1c3b7fe73b726aac5a65eb505853f92199e34
J180481 <J180481@tafe.wa.edu.au>
2024-11-18 18:14:54 +0800

    chore: changed #comments in test_sensor file to docstring comments

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -14,7 +14,7 @@ class TestSensor(unittest.TestCase):
         self.exit_sensor = ExitSensor(identity=2, is_active=True, car_park=CarPark(capacity=10, location="ridgeracer"))
 
     def test_sensor_initialized_with_all_attributes(self):
-        #testing that the sensors are initialized properly
+        """testing that the sensors are initialized properly"""
         self.assertEqual(self.entry_sensor.identity, 1)
         self.assertEqual(self.entry_sensor.is_active, True)
         self.assertIsInstance(self.entry_sensor.car_park, CarPark)
@@ -24,28 +24,28 @@ class TestSensor(unittest.TestCase):
         self.assertIsInstance(self.entry_sensor.car_park, CarPark)
 
     def test_entry_sensor_detect_vehicle(self):
-        #using random and it's built in function seed so we can generate the same "random" result for testing
+        """using random and it's built in function seed so we can generate the same "random" result for testing"""
         random.seed(0)
-        #running the script i found this is the plate number it will generate with the seed set to 0
+        """running the script i found this is the plate number it will generate with the seed set to 0"""
         plate = "FAKE-6311"
 
         self.entry_sensor.detect_vehicle()
-        #then we check to see that the plate variable is in the car_park plates attribute containing the list of plates
+        """then we check to see that the plate variable is in the car_park plates attribute containing the list of plates"""
         self.assertIn(plate, self.entry_sensor.car_park.plates)
 
     def test_exit_sensor_detect_vehicle(self):
-        #using the previous test as a reference simply for clean output i have matched the plate variable
+        """using the previous test as a reference simply for clean output i have matched the plate variable"""
         plate = "FAKE-6311"
 
-        #since we are specifically testing the exit sensor, i manually add the fake plate for this test purpose specifically
+        """since we are specifically testing the exit sensor, i manually add the fake plate for this test purpose specifically"""
         self.exit_sensor.car_park.plates = [plate]
-        #And here, i'm checking to make sure the fake plate is added
+        """And here, i'm checking to make sure the fake plate is added"""
         self.assertIn(plate, self.exit_sensor.car_park.plates)
 
-        #calling the exit sensors detect_vehicle method, which should remove the plate
+        """calling the exit sensors detect_vehicle method, which should remove the plate"""
         self.exit_sensor.detect_vehicle()
 
-        #and lastly, checking to make sure the plate is not in the list and removed as expected
+        """and lastly, checking to make sure the plate is not in the list and removed as expected"""
         self.assertNotIn(plate, self.exit_sensor.car_park.plates)
 
 
